bbed61530487fd790d7cdadde442b5bba78c94da
yihao <yihao123698745@126.com>
2018-09-29 14:33:21 +0800

    test commit

---
+++ gs-live-server/src/main/java/com/gs/subproject/user/web/TestController.java
@@ -0,0 +1,17 @@
+package com.gs.subproject.user.web;
+
+import com.gs.web.BaseResult;
+import com.gs.web.ResultUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @RequestMapping("/get")
+    public BaseResult<String> get() {
+        return ResultUtils.success("aaa");
+    }
+
+}
